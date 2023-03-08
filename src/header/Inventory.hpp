d57a566f6878df7789f9c8434f206661d182351b
frankiehuangg <frankie.huangg@gmail.com>
2023-03-08 20:22:14 +0700

    [ADD] cards to abstarct class, new getter and setter, operator function

--- src/header/Inventory.hpp
+++ src/header/Inventory.hpp
@@ -1,19 +1,30 @@
 #ifndef _INVENTORY_HPP_
 #define _INVENTORY_HPP_
 
+#include <vector>
 #include "MainCard.hpp"
 
 class Inventory {
 protected:
 	int amount;
-
+	vector<MainCard> cards;
+	
 public:
+	Inventory(int);
+
 	/***** Getter & Setter *****/
-	virtual void printCards() = 0;
+	int getAmount() const;
+	void setAmount(int);
+
+	MainCard getCard(int) const;
+	void setCard(int, MainCard);
+
+	/***** Print value *****/
+	virtual void printCards();
 
 	/***** Operator overloading *****/
-	virtual Inventory& operator+ (Inventory&) = 0;
-	virtual Inventory& operator- (Inventory&) = 0;
+	virtual Inventory& operator+(Inventory&);
+	virtual Inventory operator-(int);
 };
 
 #endif
