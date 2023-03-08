0cf98b6606953e5c834efd5150d7c0492cc315ee
frankiehuangg <frankie.huangg@gmail.com>
2023-03-08 20:34:38 +0700

    [ADD] operator+ and operator- implementation

--- src/Inventory.cpp
+++ src/Inventory.cpp
@@ -24,3 +24,26 @@ void Inventory::setCard(int index, MainCard card)
 {
 	this->cards[index] = card;
 }
+
+Inventory& Inventory::operator+(Inventory& other)
+{
+	this->amount = other.amount;
+	this->cards = other.cards;
+
+	return *this;
+}
+
+Inventory Inventory::operator-(int num)
+{
+	Inventory inventory(num);
+	
+	vector<MainCard>::iterator it = this->cards.begin();
+	for (int i = 0; i < num; i++)
+	{
+		inventory.cards[i] = this->cards[i];
+		this->cards.erase(it);
+		it++;
+	}
+
+	return inventory;
+}
