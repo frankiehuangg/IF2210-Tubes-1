129baf1fae3c61831d8e9d2b443034839d73a4fe
farizkik <fariski.mail@gmail.com>
2023-03-08 22:15:03 +0700

    [FIX] change how to add cards to table

--- src/Table.cpp
+++ src/Table.cpp
@@ -4,16 +4,13 @@ using namespace std;
 
 /***** Constructor and destructor *****/
 // Construct sebuah table dengan amount = 5, opened = 0, serta inisialisasikan atribut cards dengan panjang amount
- 
 Table::Table(Inventory& deck):Inventory(5){ 
     this->opened=0;   
-    for(int i=0;i<this->amount;i++){
-        vector<MainCard>card=deck-1;
-        (*this) + card;
-    }
+    vector<MainCard>grabbedCards=deck-5;
+    (*this) + grabbedCards;
 }
 
-// Tambahkan nilai opened dengan 1, kemudian panggil fungsi operator +
+// Tambahkan nilai opened dengan 1
 void Table::openCard(){
     this->opened +=1;
 }
