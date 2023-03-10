91d20721ad186b3e89634af2707f35e106dbd849
sozy <thinpotatogithub@gmail.com>
2023-03-10 21:49:35 +0700

    add: MainCard implementation

---
+++ src/MainCard.cpp
@@ -0,0 +1,68 @@
+#include "header/MainCard.hpp"
+
+/* Constructor & destructor */
+MainCard::MainCard(int val1, int val2)
+{
+    this->card.first = val1;
+    this->card.second = val2;
+}
+
+/***** Getter & Setter *****/
+// Value
+float MainCard::getValue() const
+{
+    return this->value;
+}
+void MainCard::setValue(float value)
+{
+    this->value = value;
+}
+
+// Number - Asumption: First element of the pair
+int MainCard::getNumber() const
+{
+    return this->card.first;
+}
+void MainCard::setNumber(int number)
+{
+    this->card.first = number;
+}
+
+// Color - Asumption: Second element of the pair
+int MainCard::getColor() const
+{
+    return this->card.second;
+}
+void MainCard::setColor(int color)
+{
+    this->card.second = color;
+}
+
+/***** Print *****/
+// Print card number and color, ends with endl
+void MainCard::printCard()
+{
+    std::cout << this->card.first << "(" << this->card.second << ")" << endl;
+}
+
+/***** Comparison *****/
+// Cek apakah nilai kartu < kartu lain
+bool MainCard::operator<(Comparable &other)
+{
+    // Not sure yet
+    return this->value < other.getValue();
+}
+
+// Cek apakah nilai kartu > kartu lain
+bool MainCard::operator>(Comparable &other)
+{
+    // Not sure yet
+    return this->value > other.getValue();
+}
+
+// Cek apakah nilai kartu = kartu lain
+bool MainCard::operator==(Comparable &other)
+{
+    // Not sure yet
+    return this->value == other.getValue();
+}
