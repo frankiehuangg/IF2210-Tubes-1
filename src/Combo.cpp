292aa0059d2b2a65fdcbef1834bca80a3507bd89
Genvictus <johannstarkmk42@gmail.com>
2023-03-11 18:56:59 +0700

    add: template for implement

---
+++ src/Combo.cpp
@@ -0,0 +1,16 @@
+#include "Combo.hpp"
+
+float getValue() const {}
+
+void setValue(float) {}
+
+/***** Comparison *****/
+// Implementasi combo pakai map
+// Cek apakah nilai kombo > kombo lain
+bool operator> (Comparable& other) {}
+
+// Cek apakah nilai kombo < kombo lain
+bool operator< (Comparable& other) {}
+
+// Cek apakah nilai kombo == kombo lain
+bool operator== (Comparable& other) {}
