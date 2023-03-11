e29a3245231a9eb14822dc3d8bda3fd4cdbb8d91
farizkik <fariski.mail@gmail.com>
2023-03-11 21:36:17 +0700

    [RFT] refactor some files

--- src/Ability_Reverse.cpp
+++ src/Ability_Reverse.cpp
@@ -3,27 +3,35 @@
 // Reverse player's turn RoundRobin 
 void useAbility(const Game &game)
 {
-	int cur_player=game.turn;
-	string cur_name=game.players[cur_player].getPlayerName();
+	int cur_player = game.turn;
+	string cur_name = game.players[cur_player].getPlayerName();
 
 	vector<Player> current;
-	for(int i=0; i<=cur_player; i++){
+	for(int i=0; i<=cur_player; i++)
+	{
 		current.push_back(game.players[i]);
 	}
-	for(int i=game.getPlayerCount();i>cur_player;i--){
+	for(int i=game.getPlayerCount(); i>cur_player; i--)
+	{
 		current.push_back(game.players[i]);
 	}
 
 	cout<<cur_name<<" melakukan reverse!"<<endl;
 	cout<<"(sisa) urutan eksekusi giliran ini : ";
-	for(int i=cur_player+1;i<=game.getPlayerCount();i++){
+
+	for(int i=cur_player+1; i<=game.getPlayerCount(); i++)
+	{
 		cout<<"<p"<<current.players[i].getPlayerNumber()<<"> "
 	}
 
 	cout<<endl;
 	cout<<"urutan giliran eksekusi selanjutnya : ";
-	for(int i=1;i<=game.getPlayerCount();i++){
-		if(i==cur_player)cout<<"<p"<<game.player[0].getPlayerNumber()<<"> ";
+	for(int i = 1; i<=game.getPlayerCount(); i++)
+	{
+		if(i == cur_player)
+		{
+			cout<<"<p"<<game.player[0].getPlayerNumber()<<"> ";
+		}
 		else{
 			cout<<"<p"<<game.player[i].getPlayerNumber()<<"> ";
 		}
@@ -31,7 +39,7 @@ void useAbility(const Game &game)
 	cout<<"<p"<<game.player[cur_player].getPlayerNumber()<<">"<<endl;
 
 	// change current round's turn
-	game.players[i]=current;
+	game.players[i] = current;
 
 	// Case needs to handle changing the round robin method
 	// karena dia ada yang ngubah urutan saat di round
