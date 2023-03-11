12c5b08e49870b3744e576ea647ea7b39f79381e
farizkik <fariski.mail@gmail.com>
2023-03-11 20:15:20 +0700

    [FIX] Reroll Ability following changes in poker

--- src/Ability_ReRoll.cpp
+++ src/Ability_ReRoll.cpp
@@ -3,13 +3,9 @@
 // Buang 2 kartu milik player sekarang, ambil ulang kartu dari deck
 void useAbility(const Game &game)
 {
-	// gimana tau player sekarang... ? help
-	// di sini dianggap ada penunjuk ke player sekarang
-	// anggap sj variabel currentPlayer
-
-	game.players[currentPlayer] - 2;		   // discard 2 cards
+	game.players[game.turn] - 2;		   // discard 2 cards
 	vector<MainCard> newCards = game.deck - 2; // grab 2 new cards from deck
-	game.players[currentPlayer] + newCards;
+	game.players[game.turn] + newCards;
 }
 
 /***** Print card *****/
@@ -24,7 +20,8 @@ void printCard()
 		cout<<"NOT AVAILABLE"<<endl;
 	}
 	else{
-		cout<<"Buang 2 kartu milik player sekarang, ambil ulang kartu dari deck"<<endl;
-	}
+		cout << "NAME     : Re-Roll" << endl;
+        cout << "STATUS   : " << this->status << endl;
+        cout << "ABILITY  : Membuang kedua kartu yang ada di tangannya dan mengambil ulang dua kartu baru dari deck" << endl;
 	*/
 }
