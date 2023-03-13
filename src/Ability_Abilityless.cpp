120294885ca67b4e11ff60e635414174c070f612
sozyGithub <thinpotatogithub@gmail.com>
2023-03-13 10:15:28 +0700

    rfc: ability

--- src/Ability_Abilityless.cpp
+++ src/Ability_Abilityless.cpp
@@ -1,13 +1,17 @@
 #include "header/Ability_Abilityless.hpp"
+#include <iostream>
+
+using namespace std;
 
 /**** Use ability, different for each card *****/
 // Ubah status abilityCard pemain lain menjadi false
-void Abilityless::useAbility(const Game &game)
+void Abilityless::useAbility(Game &game)
 {
     // Assumption we can access other player by its id from game object
     // I think this can avoid loop(?)
     // id -> new paramater of this function
 
+    // Interface
     // Example:
     /*
     if (this->status) {
@@ -16,7 +20,7 @@ void Abilityless::useAbility(const Game &game)
             game.players[id].ability.setStatus(false)
             cout << "Pemain " << game.players[id].name << " telah dinonaktifkan abilitynya." << endl;
         } else {
-            cout << "Pemain " << game.players[i].name << " tidak memiliki ability card yang dapat digunakan" << endl;
+            cout << "Pemain " << game.players[id].name << " tidak memiliki ability card yang dapat digunakan" << endl;
         }
         this->setStatus(false);
     } else {
--- src/Ability_Quadruple.cpp
+++ src/Ability_Quadruple.cpp
@@ -1,24 +1,29 @@
 #include "header/Ability_Quadruple.hpp"
+#include <iostream>
+
+using namespace std;
 
 /**** Use ability, different for each card *****/
 // Ubah status abilityCard pemain lain menjadi false
-void Quadruple::useAbility(const Game &game)
+void Quadruple::useAbility(Game &game)
 {
     // Assumption we can access other player by its id from game object
     // I think this can avoid loop(?)
     // id -> new paramater of this function
 
     // Example:
-    /*
-    if (this->status) {
+    if (!this->status)
+    {
         int initialPoint = game.point;
         game.point *= 4;
         this->setStatus(false);
+        cout << game.getPlayer().getPlayerName() << " melakukan DOUBLE!" << endl;
         cout << "Point game berubah dari " << initialPoint << " menjadi " << game.point << endl;
-    } else {
+    }
+    else
+    {
         cout << "Anda tidak dapat menggunakan ability ini." << endl;
     }
-    */
 }
 
 /***** Print card *****/
