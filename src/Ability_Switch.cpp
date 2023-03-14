266a07e4034ff40ec28417915b7e750b73e97ca7
sozyGithub <thinpotatogithub@gmail.com>
2023-03-14 15:29:58 +0700

    rfc: swap, switch

--- src/Ability_Switch.cpp
+++ src/Ability_Switch.cpp
@@ -1,29 +1,29 @@
 #include "header/Ability_Switch.hpp"
 #include "header/Game.hpp"
 
-void Switch::useAbility(Game& game) 
+void Switch::useAbility(Game &game)
 {
-    if (!this->getStatus()) 
+    if (!this->getStatus())
     {
         cout << "Oops, kartu ability switchmu telah dimatikan sebelumnya :(" << endl
              << "Silahkan lakukan perintah lain." << endl;
     }
 
-    else 
+    else
     {
         int playerCount = game.getPlayerCount();
-        Player& currentPlayer = game.getPlayer();
+        Player &currentPlayer = game.getPlayer();
         int playerOption;
 
         cout << currentPlayer.getPlayerName() << " melakukan SWITCH!" << endl;
 
         /*** Ini mungkin dijadiin satu method aja biar modular ***/
         cout << "Silahkan pilih pemain yang kartunya ingin anda tukar: " << endl;
-        for (int i = 1, num = 1; i <= playerCount; i++, num++) 
+        for (int i = 1, num = 1; i <= playerCount; i++, num++)
         {
             Player playerLoop = game.getPlayer(i);
 
-            if (playerLoop.getPlayerNumber() != currentPlayer.getPlayerNumber()) 
+            if (playerLoop.getPlayerNumber() != currentPlayer.getPlayerNumber())
             {
                 cout << num << ". " << playerLoop.getPlayerName() << endl;
             }
@@ -39,7 +39,7 @@ void Switch::useAbility(Game& game)
         }
 
         int switchedPlayerNumber = playerOption < currentPlayer.getPlayerNumber() ? playerOption : playerOption - 1;
-        Player& switchedPlayer = game.getPlayer(switchedPlayerNumber);
+        Player &switchedPlayer = game.getPlayer(switchedPlayerNumber);
 
         /*** Ini mungkin dijadiin satu method aja biar modular ***/
         MainCard temp1 = currentPlayer.getCard(0);
@@ -53,10 +53,10 @@ void Switch::useAbility(Game& game)
     }
 }
 
-void Switch::printCard() 
+void Switch::printCard()
 {
     cout << "NAME     : Switch" << endl;
-    cout << "STATUS   : " << this->status << endl;
+    cout << "STATUS   : " << (this->status ? " Belum digunakan" : " Sudah diguankan") << endl;
     cout << "ABILITY  : Jika kamu memakai Ability Card ini, kamu dapat" << endl
-         <<  "menukar kartu main card milikmu dengan kartu main card milik pemain lain" << endl; 
+         << "menukar kartu main card milikmu dengan kartu main card milik pemain lain" << endl;
 }
