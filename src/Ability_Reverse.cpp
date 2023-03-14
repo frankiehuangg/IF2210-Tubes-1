68afd86d9bf4f4c34ae331807a01b0cd022edf63
sozyGithub <thinpotatogithub@gmail.com>
2023-03-14 15:25:37 +0700

    rfc: reroll, reverse

--- src/Ability_Reverse.cpp
+++ src/Ability_Reverse.cpp
@@ -1,52 +1,53 @@
 #include "header/Ability_Reverse.hpp"
 #include "header/Game.hpp"
 
-// Reverse player's turn RoundRobin 
+// Reverse player's turn RoundRobin
 void Reverse::useAbility(Game &game)
 {
-	bool stat=false;
-	//stat=this->getStatus() error ntah kenapa
-	if (!stat) 
-    {
-        cout << "Oops, kartu ability ReRoll telah dimatikan sebelumnya :(" << endl
-             << "Silahkan lakukan perintah lain.";
-    }
+	bool stat = false;
+	// stat=this->getStatus() error ntah kenapa
+	if (!stat)
+	{
+		cout << "Oops, kartu ability ReRoll telah dimatikan sebelumnya :(" << endl
+			 << "Silahkan lakukan perintah lain.";
+	}
 	else
 	{
 		int cur_player = game.getTurn();
 		string cur_name = game.getPlayer(cur_player).getPlayerName();
 
 		vector<Player> current;
-		for(int i=0; i<=cur_player; i++)
+		for (int i = 0; i <= cur_player; i++)
 		{
 			current.push_back(game.getPlayer(i));
 		}
-		for(int i=game.getPlayerCount(); i>cur_player; i--)
+		for (int i = game.getPlayerCount(); i > cur_player; i--)
 		{
 			current.push_back(game.getPlayer(i));
 		}
 
-		cout<<cur_name<<" melakukan reverse!"<<endl;
-		cout<<"(sisa) urutan eksekusi giliran ini : ";
+		cout << cur_name << " melakukan reverse!" << endl;
+		cout << "(sisa) urutan eksekusi giliran ini : ";
 
-		for(int i=cur_player+1; i<=game.getPlayerCount(); i++)
+		for (int i = cur_player + 1; i <= game.getPlayerCount(); i++)
 		{
-			cout<<"<p"<<current[i].getPlayerNumber()<<"> ";
+			cout << "<p" << current[i].getPlayerNumber() << "> ";
 		}
 
-		cout<<endl;
-		cout<<"urutan giliran eksekusi selanjutnya : ";
-		for(int i = 1; i<=game.getPlayerCount(); i++)
+		cout << endl;
+		cout << "urutan giliran eksekusi selanjutnya : ";
+		for (int i = 1; i <= game.getPlayerCount(); i++)
 		{
-			if(i == cur_player)
+			if (i == cur_player)
 			{
-				cout<<"<p"<<game.getPlayer(0).getPlayerNumber()<<"> ";
+				cout << "<p" << game.getPlayer(0).getPlayerNumber() << "> ";
 			}
-			else{
-				cout<<"<p"<<game.getPlayer(i).getPlayerNumber()<<"> ";
+			else
+			{
+				cout << "<p" << game.getPlayer(i).getPlayerNumber() << "> ";
 			}
 		}
-		cout<<"<p"<<game.getPlayer(cur_player).getPlayerNumber()<<">"<<endl;
+		cout << "<p" << game.getPlayer(cur_player).getPlayerNumber() << ">" << endl;
 
 		// change current round's turn
 		// game.players[i] = current; ? tar deh
@@ -55,26 +56,16 @@ void Reverse::useAbility(Game &game)
 		// karena dia ada yang ngubah urutan saat di round
 		// dan ubah urutan setelah round berakhir
 
-		// It's the player's turn again 
+		// It's the player's turn again
 		game.getPlayer(cur_player).doAction(game);
 	}
-	
 }
 
 /***** Print card *****/
-// Print card info and status, if round = 1 print "NOT AVAILABLE or sum other shit idk"
+// Print card info and status
 void Reverse::printCard()
 {
-	// tidak bisa cek round, tdk ada passing parameter :(
-	// MUNGKIN klo butuh jadi void printCard(const Game&) ?
-	// jadi implemennya
-	/*
-	if(game.round==1){
-		cout<<"NOT AVAILABLE"<<endl;
-	}
-	else{
-		cout << "NAME     : Reverse" << endl;
-        cout << "STATUS   : " << this->status << endl;
-        cout << "ABILITY  : Memutar arah giliran eksekusi perintah oleh pemain." << endl;
-	*/
+	std::cout << "NAME     : Reverse" << std::endl;
+	std::cout << "STATUS   : " << (this->status ? " Belum digunakan" : " Sudah diguankan") << std::endl;
+	std::cout << "ABILITY  : Memutar arah giliran eksekusi perintah oleh pemain." << std::endl;
 }
